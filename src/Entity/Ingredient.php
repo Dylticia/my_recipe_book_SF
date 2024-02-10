cec1b23b4cb6de287b95dcc8b3d01ead0fc20473
Nany <lesfarguettes@free.fr>
2024-02-10 17:47:11 +0100

    save_1

--- src/Entity/Ingredient.php
+++ src/Entity/Ingredient.php
@@ -5,6 +5,8 @@ namespace App\Entity;
 use App\Repository\IngredientRepository;
 use Doctrine\ORM\Mapping as ORM;
 
+use Symfony\Component\Validator\Constraints as Assert;
+
 #[ORM\Entity(repositoryClass: IngredientRepository::class)]
 class Ingredient
 {
@@ -14,9 +16,11 @@ class Ingredient
     private ?int $id = null;
 
     #[ORM\Column(length: 50)]
+    #[Assert\Length(min: 2, max: 50)]
     private ?string $name = null;
 
     #[ORM\Column]
+    #[Assert\LessThan(200)]
     private ?float $price = null;
 
     #[ORM\Column]
